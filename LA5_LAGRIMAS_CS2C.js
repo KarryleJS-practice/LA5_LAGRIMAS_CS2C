4fe95fc66f5346b6ca2a34e6ae266258e1ac9e9e
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-09-23 10:56:42 +0800

    Create LA5_LAGRIMAS_CS2C.js

---
+++ LA5_LAGRIMAS_CS2C.js
@@ -0,0 +1,14 @@
+//Comparison or Relational Operator
+let firstFavNumb = 20;
+let secondFavNumb = 07;
+
+console.log(firstFavNumb > secondFavNumb) ; 
+console.log(firstFavNumb < secondFavNumb) ;
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
+console. log("firstFavNumb:", firstFavNumb);
+console.log("secondFavNumb", secondFavNumb);
